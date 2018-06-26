e23aa657225298e3b2a5f36f2ee78ceed4e1dbd6
DmytroPashynskyi <d.pashynskyi@globalgames.net>
2018-06-26 13:23:22 +0300

    Added (!) scripts to enable/disable bots

---
+++ src/script/enable_bots.js
@@ -0,0 +1,24 @@
+import Mongo from '../lib/mongo'
+import config from '../../config/common'
+import fs from 'fs'
+import readline from 'readline'
+
+main().catch(console.error)
+
+async function main() {
+  
+  const lineReader = readline.createInterface({ input: fs.createReadStream(config.skinsOnBots.inputFile) })
+  const logins = []
+  lineReader.on('line', line => logins.push(line.replace(/[\"\'\,]/g, '').trim()))
+  lineReader.on('close', async () => {
+    
+    const mongo = new Mongo({})
+    await mongo.getDb()
+    
+    await mongo.update('bots', { login: { $nin: logins } }, { $set: { isActive: true } })
+    
+    await mongo.close()
+    
+  })
+  
+}
